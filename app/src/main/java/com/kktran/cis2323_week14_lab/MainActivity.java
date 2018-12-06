161e9915f613f74e076e6cd1d551fe4ab3888acf
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-12-05 20:19:31 -0600

    android app to do list

--- app/src/main/java/com/kktran/cis2323_week14_lab/MainActivity.java
+++ app/src/main/java/com/kktran/cis2323_week14_lab/MainActivity.java
@@ -2,6 +2,10 @@ package com.kktran.cis2323_week14_lab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBHelper db = new DBHelper(this);
+        ListView lstTask = findViewById(R.id.lstTask);
+
+        ArrayList<String> taskList = db.getTaskList();
+        ArrayAdapter<String> mArrayAdaptor = new ArrayAdapter<String>(this,R.layout.row, R.id.TaskTitle, taskList);
+        lstTask.setAdapter(mArrayAdaptor);
     }
 }
